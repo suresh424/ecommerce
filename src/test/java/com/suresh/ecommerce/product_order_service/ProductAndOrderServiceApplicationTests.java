a9fa512d2bdf14ffe87bea871c0d515b667184f2
SureshB <Suresh.Bandaru@talentica.com>
2025-11-13 16:05:46 +0530

    added initial Spring Boot project setup

---
+++ src/test/java/com/suresh/ecommerce/product_order_service/ProductAndOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suresh.ecommerce.product_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAndOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
